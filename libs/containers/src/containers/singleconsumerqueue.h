b622b2f1268a8109bdd227eb186dff4d2a376a6a
Marten van de Sanden <mart.vd.sanden@gmail.com>
2022-02-14 11:41:20 +0100

    DEV: Small documentation update.

--- libs/containers/src/containers/singleconsumerqueue.h
+++ libs/containers/src/containers/singleconsumerqueue.h
@@ -5,6 +5,19 @@
 
 namespace eco {
 namespace containers {
+    /**
+     * @brief Single consumer queue
+     * 
+     * This is a queue to which multiple threads can push concurrently, but is only safe to
+     * pop from one thread at a time.
+     * 
+     * Currently this uses the `AsyncQueue`, which is a multiple consumer, multiple producer
+     * queue and therefor has stronger requirements than a single consumer queue. In the future
+     * an optimized single consumer queue might be provided here.
+     * 
+     * TODO: Encapsulate the AsyncQueue instead of typedef so we can provide a correct contract
+     *       for this type.
+     */
     template <typename T>
     using SingleConsumerQueue = AsyncQueue<T>;
 }
